ce52564fcfab3187f2681aeb5092c2d29d95983d
Adit Sinha <79353608+adit-sinha@users.noreply.github.com>
2024-09-23 18:06:24 -0600

    added most components

---
+++ Project/src/app/portfolio-models.ts
@@ -0,0 +1,32 @@
+export const P_MODALS = [
+    {
+        id: 'p1',
+        img: 'cabin.png',
+        text: '',
+    },
+    {
+        id: 'p2',
+        img: 'cake.png',
+        text: '',
+    },
+    {
+        id: 'p3',
+        img: 'circus.png',
+        text: '',
+    },
+    {
+        id: 'p4',
+        img: 'game.png',
+        text: '',
+    },
+    {
+        id: 'p5',
+        img: 'safe.png',
+        text: '',
+    },
+    {
+        id: 'p6',
+        img: 'submarine.png',
+        text: '',
+    }
+]

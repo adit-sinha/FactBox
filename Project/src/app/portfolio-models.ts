c710e49d39824a0d4b7b8cf87e1a10ceb4cedb6c
Adit Sinha <79353608+adit-sinha@users.noreply.github.com>
2024-10-04 10:02:11 -0600

    updating images

--- Project/src/app/portfolio-models.ts
+++ Project/src/app/portfolio-models.ts
@@ -1,37 +1,39 @@
 export const P_MODALS = [
     {
         id: 'p1',
-        img: 'cabin.png',
+        img: 'science.png',
         text: 'This is 1',
         title: 'Science'
     },
     {
         id: 'p2',
-        img: 'cake.png',
+        img: 'sports.png',
         text: 'This is 2',
         title: 'Sports'
     },
     {
         id: 'p3',
-        img: 'circus.png',
+        img: 'video-games.png',
+        //the first three pictures belong to https://www.freepik.com/
         text: 'This is 3',
         title: 'Video Games'
     },
     {
         id: 'p4',
-        img: 'game.png',
+        img: 'music.png',
         text: 'This is 4',
         title: 'Music'
     },
     {
         id: 'p5',
-        img: 'safe.png',
+        img: 'mythology.png',
         text: 'This is 5',
         title: 'Mythology'
     },
     {
         id: 'p6',
-        img: 'submarine.png',
+        img: 'film.png',
+        //pictures 4 to 6 (inclusive) belong to https://www.vecteezy.com/ 
         text: 'This is 6',
         title: 'Film'
     }
